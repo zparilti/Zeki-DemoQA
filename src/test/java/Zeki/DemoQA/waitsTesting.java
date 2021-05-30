c586e594a12a17f37a98125c3603cfc161905a4c
zparilti <zparilti@gmail.com>
2021-05-30 13:45:31 -0400

    implicit wait example is added

--- src/test/java/Zeki/DemoQA/waitsTesting.java
+++ src/test/java/Zeki/DemoQA/waitsTesting.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class waitsTesting {
 
     @Test
@@ -20,19 +22,25 @@ public class waitsTesting {
 
         driver.get("https://demoqa.com/dynamic-properties");
 
+        // an example for implicit wait
+        driver.manage().timeouts().implicitlyWait(7000, TimeUnit.MILLISECONDS);
+        WebElement seeyouLater = driver.findElement(By.xpath("//button[@id='visibleAfter']"));
 
+        // an example for explicit wait
         //1WebElement timedenabledButton = driver.findElement(By.xpath("//button[@id='enableAfter']"));
         //1Boolean buttonstatus = timedenabledButton.isEnabled();
         //1System.out.println("buttonstatus = " + buttonstatus);
         //1timedenabledButton.click(); // tried to click here
         //WebDriverWait waitforMe = new WebDriverWait(driver,10);
-        WebDriverWait waitforYou = new WebDriverWait(driver,1);
+        WebDriverWait waitforYou = new WebDriverWait(driver,10);
         //1waitforYou.until(ExpectedConditions.elementToBeClickable(timedenabledButton));
 
         waitforYou.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='visibleAfter']")));
-        WebElement seeyouLater = driver.findElement(By.xpath("//button[@id='visibleAfter']"));
+        seeyouLater = driver.findElement(By.xpath("//button[@id='visibleAfter']"));
         Boolean visibleNot = seeyouLater.isDisplayed();
         System.out.println("visibleNot = " + visibleNot);
+
+        //
 
 
     }
